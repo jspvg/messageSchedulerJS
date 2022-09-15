961802f10e93b5b64d720268ad2a4075d069c276
jspvg <josipovic1501@gmail.com>
2022-09-15 14:07:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const scheduledMessages = [
+    {
+        id: 1,
+        message: 'Kraj casa!',
+        time: {
+            hours: '00',
+            minutes: '00',
+            seconds: '05'
+        }
+    },
+    {
+        id: 2,
+        message: 'Happy New Year!',
+        time: {
+            hours: '00',
+            minutes: '10',
+            seconds: '05'
+        }
+    },
+
+];
